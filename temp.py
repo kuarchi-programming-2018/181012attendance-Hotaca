689522fe645999bbe49b9d0e5bed39034414d615
Hotaca <iwami.hotaka.38s@st.kyoto-u.ac.jp>
2018-10-12 17:26:23 +0900

    update test

---
+++ temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print(5*5)
+
+def fib(n):
+    a , b = 0 ,1
+    for i in range (n):
+        a , b = b , a+b
+    return b
+
+def fib(n):
+    return n
